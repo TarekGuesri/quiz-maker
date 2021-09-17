5e6975512c8a7ab3c58364fd274c7a874e8a049c
TarekGuesri <tguesri@gmail.com>
2021-09-17 20:58:31 +0100

    Added basic express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,36 @@
-console.log('test');
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const rateLimit = require("express-rate-limit");
+const helmet  = require('helmet');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Enabling cors
+app.use(cors());
+
+// Enabling helmet
+app.use(helmet());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limiting each IP to 100 requests per windowMs
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100
+});
+
+app.listen(PORT, () =>
+  console.log(
+    colors.yellow.bold(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  )
+);
