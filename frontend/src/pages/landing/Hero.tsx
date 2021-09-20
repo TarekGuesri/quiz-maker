52abaca1a06a3b832f47ecc1c22df37bc4358e29
TarekGuesri <tguesri@gmail.com>
2021-09-20 19:59:08 +0100

    Removed unused component

--- frontend/src/pages/landing/Hero.tsx
+++ frontend/src/pages/landing/Hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@mui/styles';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
