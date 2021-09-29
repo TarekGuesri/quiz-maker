45c7157a02c6a2d86f129aac4f563a1f80711498
TarekGuesri <tguesri@gmail.com>
2021-09-29 18:06:58 +0100

    Finished transfering the state of create-quiz page to redux

--- frontend/src/redux/create-quiz/create-quiz-slice.ts
+++ frontend/src/redux/create-quiz/create-quiz-slice.ts
@@ -64,7 +64,7 @@ export const createQuizSlice = createSlice({
       state.selectedAnswers.push(firstAnswerID);
 
       // Changing index
-      state.questionIndex++;
+      state.questionIndex = current(state.questions).length - 1;
     },
     removeQuestion: (state) => {
       // We remove the selected answer from the target question
