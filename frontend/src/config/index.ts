01d4b629775cf7aa4d4d0be73007895b5c43ef4b
TarekGuesri <tguesri@gmail.com>
2021-10-04 17:00:05 +0100

    Added a config file

---
+++ frontend/src/config/index.ts
@@ -0,0 +1,20 @@
+import { Environment } from "../types";
+
+// Environment getter
+export const getEnv: () => Environment = () => {
+  switch (location.hostname) {
+    case "localhost":
+      return "development";
+    default:
+      return "production";
+  }
+};
+
+const environments: Environment[] = ["development", "production"];
+const e = environments.indexOf(getEnv());
+
+export const config = {
+  api: {
+    url: [`http://localhost:3000`, "http://localhost:3000"][e],
+  },
+};
