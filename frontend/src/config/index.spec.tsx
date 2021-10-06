83f231785232c290add6635e5676474ea8188b2b
TarekGuesri <tguesri@gmail.com>
2021-10-06 15:28:45 +0100

    Added config test

---
+++ frontend/src/config/index.spec.tsx
@@ -0,0 +1,23 @@
+import { cleanup } from "@testing-library/react";
+import { config, getEnv } from ".";
+import { Environment } from "src/types";
+
+describe("src/config/index.ts", () => {
+  afterEach(() => cleanup());
+
+  test("getEnv return development when location.hostname is localhost", () => {
+    expect(getEnv()).toBe<Environment>("development");
+  });
+
+  test("getEnv return production when location.hostname is not localhost", () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete (window as any).location;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window.location as any) = new URL("https://github.com/TarekGuesri");
+    expect(getEnv()).toBe<Environment>("production");
+  });
+
+  test("config is defined", () => {
+    expect(config).toBeDefined();
+  });
+});
