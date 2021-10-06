59c0ab6db4c8ab149b743eb274def66ddec5d5c4
TarekGuesri <tguesri@gmail.com>
2021-10-06 15:13:07 +0100

    Removed comments

--- frontend/src/redux/ui/ui-slice.ts
+++ frontend/src/redux/ui/ui-slice.ts
@@ -8,7 +8,6 @@ const initialState: UIState = {
 export const uiSlice = createSlice({
   name: "ui",
   initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     switchDarkMode: (state) => {
       state.darkMode = !state.darkMode;
