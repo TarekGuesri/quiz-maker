39077a1fef0445e9ca14ec9f39a9c71078a04d15
TarekGuesri <tguesri@gmail.com>
2021-10-11 02:28:47 +0100

    Changed split server to app and server

---
+++ backend/src/app.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+import helmet from "helmet";
+import hpp from "hpp";
+import rateLimit from "express-rate-limit";
+import mongoSanitize from "express-rate-limit";
+
+import { connectDB } from "./config/db";
+
+import { restRouter } from "./routes/rest";
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+export const app = express();
+
+// Enabling cors
+app.use(cors());
+
+// Enabling helmet
+app.use(helmet());
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Limiting each IP to 100 requests per windowMs
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+});
+app.use(limiter);
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// body parsing middleware
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Defining Routes
+app.use("/rest", restRouter);
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import colors from "colors";
 import { MONGODB_URI } from "../utils/secrets";
 
 export const connectDB = async (): Promise<void> => {
   const conn = await mongoose.connect(MONGODB_URI || "");
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  console.log(
+    colors.cyan.underline.bold(`MongoDB Connected: ${conn.connection.host}`),
+  );
 };
