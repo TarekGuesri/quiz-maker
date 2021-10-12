ac63e573bc8c5d47995b1a7c7a55931f603248c9
TarekGuesri <tguesri@gmail.com>
2021-10-12 17:34:45 +0100

    Covered all lines in testing

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,14 +2,20 @@ import mongoose from "mongoose";
 import colors from "colors";
 import { MONGODB_URI } from "./secrets";
 
-export const connectDB = async (): Promise<void> => {
-  const conn = await mongoose.connect(MONGODB_URI || "");
+// Disabling logs when doing testing
+/* istanbul ignore else */
+if (process.env.NODE_ENV === "test") {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  console.log = () => {};
+}
 
-  if (process.env.NODE_ENV !== "test") {
-    console.log(
-      colors.cyan.underline.bold(`MongoDB Connected: ${conn.connection.host}`),
-    );
-  }
+export const connectDB = async (): Promise<void> => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const conn = await mongoose.connect(MONGODB_URI!);
+
+  console.log(
+    colors.cyan.underline.bold(`MongoDB Connected: ${conn.connection.host}`),
+  );
 };
 
 export const disconnectDB = (): void => {
