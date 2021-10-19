d70795dde7dd62e1b5b5ad5798552e526a849bfc
TarekGuesri <tguesri@gmail.com>
2021-10-19 18:45:27 +0100

    Cleaned asyncHandler code

--- backend/src/middleware/async.ts
+++ backend/src/middleware/async.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
+interface AsyncMiddleware {
+  (req: Request, res: Response, next: NextFunction): Promise<unknown>;
+}
+
 export const asyncHandler =
-  (fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>) =>
-  (req: Request, res: Response, next: NextFunction): Promise<unknown> =>
+  (fn: AsyncMiddleware): AsyncMiddleware =>
+  (req, res, next) =>
     Promise.resolve(fn(req, res, next)).catch(next);
