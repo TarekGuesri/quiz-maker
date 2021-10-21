e60bd7fb20c09ef47e24f9afb6456d64381f2378
TarekGuesri <tguesri@gmail.com>
2021-10-21 19:10:34 +0100

    Added error testing

--- backend/src/middleware/error.spec.ts
+++ backend/src/middleware/error.spec.ts
@@ -18,7 +18,7 @@ const mockResponse = (): Response => {
 const next = jest.fn();
 
 describe("src/middleware/error.spec.ts", () => {
-  test("Mongoose bad ObjectId should return 404 with Resource not found message", async () => {
+  test("Mongoose bad ObjectId should return 404 with 'Resource not found' message", async () => {
     const error = {
       name: "CastError",
     };
@@ -31,6 +31,56 @@ describe("src/middleware/error.spec.ts", () => {
     expect(res.status).toHaveBeenCalledWith(404);
     expect(res.json).toHaveBeenCalledWith({
       error: "Resource not found",
+      success: false,
+    });
+  });
+
+  test("Mongoose duplicate key should return 400 with 'Duplicate field value entered' message", async () => {
+    const error = {
+      code: 11000,
+    };
+
+    const req = mockRequest();
+    const res = mockResponse();
+
+    errorHandler(error, req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      error: "Duplicate field value entered",
+      success: false,
+    });
+  });
+
+  test("Mongoose validation error should return 400 with a message that contains the fields' error messages", async () => {
+    const error = {
+      name: "ValidationError",
+      errors: [{ message: "Test Error 1" }, { message: "Test Error 2" }],
+    };
+
+    const req = mockRequest();
+    const res = mockResponse();
+
+    errorHandler(error, req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      error: "Test Error 1. Test Error 2",
+      success: false,
+    });
+  });
+
+  test("Unhandled errors should return 500 with 'Server Error' message", async () => {
+    const error = {};
+
+    const req = mockRequest();
+    const res = mockResponse();
+
+    errorHandler(error, req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      error: "Server Error",
       success: false,
     });
   });
