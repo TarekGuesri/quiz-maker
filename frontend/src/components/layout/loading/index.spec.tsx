476dbe2d94e095e7b0e92b132c28ec0873ba681a
TarekGuesri <tguesri@gmail.com>
2021-10-26 21:49:00 +0100

    Fixed Loading test

--- frontend/src/components/layout/loading/index.spec.tsx
+++ frontend/src/components/layout/loading/index.spec.tsx
@@ -5,15 +5,19 @@ import React from "react";
 import { cleanup, render } from "@testing-library/react";
 import { Loading } from ".";
 import { Theme } from "src/components/theme";
+import { Provider } from "react-redux";
+import { store } from "src/redux/store";
 
 describe("src/components/loading/index.tsx", () => {
   afterEach(() => cleanup());
 
   test("Render Loading component", () => {
     const renderedComponent = render(
-      <Theme>
-        <Loading />
-      </Theme>,
+      <Provider store={store}>
+        <Theme>
+          <Loading />
+        </Theme>
+      </Provider>,
     );
 
     expect(renderedComponent.container).toMatchSnapshot();
