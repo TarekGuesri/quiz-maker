f8aac0a1f4aeed32613864238972c2d25a66cbf7
TarekGuesri <tguesri@gmail.com>
2021-10-26 21:26:46 +0100

    Updated snapshots and footer test component

--- frontend/src/components/layout/footer/index.spec.tsx
+++ frontend/src/components/layout/footer/index.spec.tsx
@@ -1,44 +1,13 @@
 import React from "react";
-import { cleanup, render, screen, fireEvent } from "@testing-library/react";
-import { Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "src/redux/store";
-import { history } from "src/utils/history";
-import { Navbar } from ".";
+import { cleanup, render } from "@testing-library/react";
+import { Footer } from ".";
 
 describe("src/components/layout/navbar", () => {
   afterEach(() => cleanup());
 
-  test("Render Navbar component", () => {
-    const renderedComponent = render(
-      <Provider store={store}>
-        <Router history={history}>
-          <Navbar />
-        </Router>
-      </Provider>,
-    );
+  test("Render Fotter component", () => {
+    const renderedComponent = render(<Footer />);
 
     expect(renderedComponent.container).toMatchSnapshot();
-  });
-
-  test("Check if dark mode changes after clicking the switch", () => {
-    render(
-      <Provider store={store}>
-        <Router history={history}>
-          <Navbar />
-        </Router>
-      </Provider>,
-    );
-
-    const {
-      ui: { darkMode: currentDarkMode },
-    } = store.getState();
-
-    const darkModeSwitch = screen.getByRole("checkbox", { name: /🌙/i });
-
-    // Clicking on the switch button
-    fireEvent.click(darkModeSwitch);
-
-    expect(store.getState().ui.darkMode).toEqual(!currentDarkMode);
   });
 });
--- frontend/src/components/layout/loading/index.spec.tsx
+++ frontend/src/components/layout/loading/index.spec.tsx
@@ -1,16 +1,20 @@
 /**
  * @jest-environment jsdom
  */
-
+import React from "react";
 import { cleanup, render } from "@testing-library/react";
 import { Loading } from ".";
-import React from "react";
+import { Theme } from "src/components/theme";
 
 describe("src/components/loading/index.tsx", () => {
   afterEach(() => cleanup());
 
   test("Render Loading component", () => {
-    const renderedComponent = render(<Loading />);
+    const renderedComponent = render(
+      <Theme>
+        <Loading />
+      </Theme>,
+    );
 
     expect(renderedComponent.container).toMatchSnapshot();
   });
