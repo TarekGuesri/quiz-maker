e950092df12fbdc5b306e5513b7f346feb55eb19
TarekGuesri <tguesri@gmail.com>
2021-11-03 19:08:54 +0100

    Completed frontend testing

--- frontend/src/redux/quiz/quiz-slice.spec.ts
+++ frontend/src/redux/quiz/quiz-slice.spec.ts
@@ -1,8 +1,14 @@
-import React from "react";
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup } from "@testing-library/react";
 import { store } from "src/redux/store";
 import axios from "axios";
-import { getQuizByCode, getQuizResult, resetState } from "./quiz-slice";
+import {
+  getQuizByCode,
+  getQuizResult,
+  setQuiz,
+  nextQuestion,
+  resetState,
+} from "./quiz-slice";
+import { Quiz } from "src/types";
 
 jest.mock("axios");
 
@@ -12,8 +18,8 @@ describe("src/redux/quiz/quiz-slice.ts", () => {
     store.dispatch(resetState());
   });
 
-  test("getQuizByCode should give 'Something unexpected happened' when error has no response", async () => {
-    // Getting failed requestion without a repsonse value
+  test("getQuizByCode should make errorMessage as error response and 'Something unexpected happend!' when it's not defined", async () => {
+    // Getting failed requestion with a repsonse value
     (axios as jest.Mocked<typeof axios>).get.mockRejectedValue({
       response: { data: "test" },
     });
@@ -29,5 +35,59 @@ describe("src/redux/quiz/quiz-slice.ts", () => {
     expect(store.getState().quiz.errorMessage).toBe(
       "Something unexpected happend!",
     );
+  });
+
+  test("getQuizResult should set quizResult on successful request", async () => {
+    // Setting the quiz
+    await store.dispatch(setQuiz({ code: "test" } as Quiz));
+
+    // Getting failed requestion without a repsonse value
+    (axios as jest.Mocked<typeof axios>).post.mockResolvedValue({
+      data: 50,
+    });
+
+    await store.dispatch(getQuizResult());
+
+    expect(store.getState().quiz.quizResult).toBe(50);
+  });
+
+  test("getQuizResult should make errorMessage as error response and 'Something unexpected happend!' when it's not defined", async () => {
+    // Setting the quiz
+    await store.dispatch(setQuiz({ code: "test" } as Quiz));
+
+    // Getting failed requestion with a repsonse value
+    (axios as jest.Mocked<typeof axios>).post.mockRejectedValue({
+      response: { data: "test" },
+    });
+
+    await store.dispatch(getQuizResult());
+
+    expect(store.getState().quiz.errorMessage).toBe("test");
+
+    // Getting failed requestion without a repsonse value
+    (axios as jest.Mocked<typeof axios>).post.mockRejectedValue({});
+
+    await store.dispatch(getQuizResult());
+
+    expect(store.getState().quiz.errorMessage).toBe(
+      "Something unexpected happend!",
+    );
+  });
+
+  test("nextQuestion shouldn't change questionIndex when it's the last page", async () => {
+    // Setting the quiz
+    await store.dispatch(
+      setQuiz({ questions: [{ id: "1" }, { id: " 2" }] } as Quiz),
+    );
+
+    expect(store.getState().quiz.questionIndex).toBe(0);
+
+    await store.dispatch(nextQuestion());
+
+    expect(store.getState().quiz.questionIndex).toBe(1);
+
+    await store.dispatch(nextQuestion());
+
+    expect(store.getState().quiz.questionIndex).toBe(1);
   });
 });
