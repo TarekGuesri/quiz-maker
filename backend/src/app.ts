245b3b206fcdc86c1914d927fffecf61229d6777
TarekGuesri <tguesri@gmail.com>
2021-12-23 19:57:14 +0100

    Fixed production build problem

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -61,7 +61,7 @@ app.use("/rest", restRouter);
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html")),
